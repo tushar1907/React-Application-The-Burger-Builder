4ff998dd6eb9d33475272b7ccba4fc673cd9476e
Tushar <gupta.tus@husky.neu.edu>
2018-09-19 17:58:48 -0400

    the roof of the application done

--- the-burger-builder/src/App.js
+++ the-burger-builder/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 
-
+import Layout from './components/Layout/Layout'
 
 class App extends Component {
   render() {
     return (
-      <div>
-       Tushar
-      </div>
+      <Layout>
+        <div>
+          Tushar
+        </div>
+      </Layout>
+
     );
   }
 }
